b5b1d0bc10b354c7aea4a89815a2c78352cd3711
EdvaldoJose <edvaldojose003@gmail.com>
2025-01-24 22:16:18 -0300

    Correcao dos test 2

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const controller = {};
 
   controller.get = async (req, res) => {
-    const responseString = `Versao 2025 da Bia ${process.env.VERSAO_API || "3.2.0"}`;
+    const responseString = `Bia ${process.env.VERSAO_API || "3.2.0"}`;
     res.send(responseString);
   };
 
